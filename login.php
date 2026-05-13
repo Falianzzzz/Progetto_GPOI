b2d574e4f2872381448a3f1f5317370299587bc5
student <student@itismeucci.com>
2026-05-13 10:46:12 +0200

    form

--- login.php
+++ login.php
@@ -1,2 +1,18 @@
 <?php 
+
+echo "
+<form action='login.php' method='post'>
+Username: <input type='text' name='username' required><br>
+Password: <input type='text' name='password' required><br>
+<buttton type='submit'>Invia</button>
+</form>";
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    $user = trim($_POST["username"]);
+    $pass = trim($_POST["password"]);
+
+}
+
 ?>
